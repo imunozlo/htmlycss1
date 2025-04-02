45ed292c332ad88c0adba1c0e744db16b271de2f
Iván Muñoz López <imunozlo@uoc.edu>
2025-04-02 20:01:56 +0200

    Commit final PAC1

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const mainSection = document.querySelector('.main-section');
+    
+    if (mainSection) {
+      mainSection.style.opacity = 0;
+      mainSection.offsetHeight;
+      // Cambia la opacidad a 1 para lograr un fade-in
+      mainSection.style.transition = 'opacity 1s ease-in';
+      mainSection.style.opacity = 1;
+    }
+  });
+  
